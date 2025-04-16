3c47f03bfed149985ed7e6e99f516da98b8ecc92
Ruth Wanjiru <ruthmwaniki960@gmail.com>
2025-04-16 18:50:26 +0300

    Updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function Header() {
   return (
     <View style={styles.headerContainer}>
-      <Text style={styles.headerText}>Equal Gender</Text>
+      <Text style={styles.headerText}>EquaGender</Text>
       <TouchableOpacity style={styles.menuButton}>      
       <MaterialIcons name="more-vert" size={30} color="white" />
       </TouchableOpacity>
@@ -19,11 +19,11 @@ const styles = StyleSheet.create({
   headerContainer: {
     backgroundColor: 'rgb(96, 6, 138)',
     padding: 20,
-    height: 130,
+    height: 100,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginTop: 10,
+    
     borderTopLeftRadius: 30, 
     borderTopRightRadius: 30,
     elevation: 10, // Add shadow for Android  
